e544ae0244547c2f1807cd2007f66cbc973fea3e
Ted-Rose <tedis.rozenfelds@gmail.com>
2023-09-22 04:55:16 -0700

    68. Storing Data and Validation

--- event-management/app/Http/Controllers/Api/EventController.php
+++ event-management/app/Http/Controllers/Api/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class EventController extends Controller
 {
@@ -19,8 +20,31 @@ class EventController extends Controller
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        //
+    {  
+        $request->validate([
+          // If in validation any error occurs the user will be
+          // redirected to the previous page (Laravel starting page
+          // for example)
+          'name' => 'required|string|max:255',
+          'description' => 'nullable|string',
+          'start_time' => 'required|date',
+          // End date should be after the start_date
+          'end_time' => 'required|date|after:start_time',
+        ]);
+
+        $event = Event::create([
+          // !!!! Event requires the user column to be set, but
+          // since we have some users loaded we will for now
+          // set all created events by default to be assocaited
+          // to user_id 1
+          'user_id' => 1,
+          'name' => $request->name,
+          'description' => $request->description,
+          'start_time' => $request->start_time,
+          'end_time' => $request->end_time,          
+        ]); 
+        
+        return $event;
     }
 
     /**
