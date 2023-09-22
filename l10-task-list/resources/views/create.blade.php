5e98c5171676f75148667f52495ee30d20f7918c
Ted-Rose <tedis.rozenfelds@gmail.com>
2023-09-22 00:21:16 -0700

    33. Keeping Old Values in the Form

--- l10-task-list/resources/views/create.blade.php
+++ l10-task-list/resources/views/create.blade.php
@@ -20,10 +20,10 @@ if you submit empty form --}}
   <form method="POST" action="{{ route('tasks.store') }}">
     @csrf
     <div>
-      <label for="title">
+      <label for="title" value="{{ old('title') }}">
         Title
       </label>
-      <input text="text" name="title" id="title" />
+      <input text="text" name="title" id="title"  value="{{ old('title') }}"/>
       {{-- Displays the error message right below input --}}
       @error('title')
         <p class="error-message">{{ $message }}</p>
@@ -32,7 +32,9 @@ if you submit empty form --}}
 
     <div>
       <label for="description">Description</label>
-      <textarea name="description" id="description" rows="5"></textarea>
+      {{-- The old helper works only with forms that are sent using POST method.
+      DON'T USE OLD METHOD WITH CREDENTIALS!!!! --}}
+      <textarea name="description" id="description" rows="5">{{ old('description') }}</textarea>
       @error('description')
         <p class="error-message">{{ $message }}</p>
       @enderror
@@ -40,7 +42,7 @@ if you submit empty form --}}
 
     <div>
       <label for="long_description">Long Description</label>
-      <textarea name="long_description" id="long_description" rows="10"></textarea>
+      <textarea name="long_description" id="long_description" rows="10">{{ old('long_description') }}</textarea>
       @error('long_description')
         <p class="error-message">{{ $message }}</p>
       @enderror
